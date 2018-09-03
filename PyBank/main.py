8daf65812a603de4a58581a1bc5346b6000aec97
sallad4 <dallas.gold@gmail.com>
2018-09-03 11:40:11 -0700

    git push

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,26 @@
 import csv
+import statistics
+
+#def findDataCount(bankData):
+    #bankDataCount = csv.reader(bankData, delimiter=",")
+    
+    #count = 0
+
+    #for _ in bankData:
+    #    count += 1
+
+    #print(count)
+
+#def findPLTotal(bankData):
+    #bank_PL_data = csv.reader(bankData, delimiter=",")
+    #next(bankData)
+
+    #plTotal = 0
+
+    #for row in bankData:
+    #   plTotal += int(row[1])
+    
+    #print(plTotal)
 
 #The total number of months included in the dataset
 #The total net amount of "Profit/Losses" over the entire period
@@ -6,29 +28,83 @@ import csv
 #The greatest increase in profits (date and amount) over the entire period
 #The greatest decrease in losses (date and amount) over the entire period
 
+#with open('pyBank/budget_data.csv', 'r') as csvfile:
+#    bank_data = csv.DictReader(csvfile, delimiter=",")
+    #next(bank_data)
+    
+#    for row in bank_data:
+#        print(row['Profit/Losses'])
+
 with open('pyBank/budget_data.csv', 'r') as csvfile:
-    bank_data = csv.reader(csvfile, delimiter=",")
-
-    csv_header = next(bank_data)
-
-    #row_count = sum(1 for row in bank_data)
+    bank_data = csv.DictReader(csvfile, delimiter=",")
     
     count = 0
 
-    for row in bank_data:
+    for _ in bank_data:
         count += 1
 
     print(count)
 
-    #print total row count
-    #print(row_count)
-
+with open('pyBank/budget_data.csv', 'r') as csvfile:
+    bank_data = csv.DictReader(csvfile, delimiter=",")    
+    
     plTotal = 0
 
     for row in bank_data:
-        plTotal += int(row[1])
-        
+        plTotal += int(row['Profit/Losses'])
+    
     print(plTotal)
 
+with open('pyBank/budget_data.csv', 'r') as csvfile:
+    bank_data = csv.DictReader(csvfile, delimiter=",")     
+    
+    monthly_PL_List = []
+
+    for row in bank_data:
+        monthly_PL_List.append(int(row['Profit/Losses']))
+    
+    avgPL = statistics.mean(monthly_PL_List)
+    
+    print(avgPL)
+
+with open('pyBank/budget_data.csv', 'r') as csvfile:
+    bank_data = csv.DictReader(csvfile, delimiter=",")     
+    
+    maxList, minList, dateList = [], [], []
+
+    for row in bank_data:
+        maxList.append(int(row['Profit/Losses']))
+        minList.append(int(row['Profit/Losses']))
+        dateList.append(row['Date'])
+        
+    maxMonth = max(maxList)
+    minMonth = min(minList)
+
+    counter = 0
+
+    for i in maxList:
+        if int(i) == maxMonth:    
+            print(dateList[counter] + " " + str(maxMonth))
+            break 
+        counter +=1
+
+    counter = 0 
+    
+    for i in minList:
+        if int(i) == minMonth:    
+            print(dateList[counter] + " " + str(minMonth))
+            break 
+        counter +=1
+
+    #for i in maxList:
+    ##    if maxMonth == int(i):
+    #        print((dateList[i]))
+    #for _ in minList:
+    #    if minMonth == int(_['Profit/Losses']):
+    #        print('dateList'[_])
+
+ 
+    #findDataCount(bank_data)
+    #findPLTotal(bank_data)
 
     
