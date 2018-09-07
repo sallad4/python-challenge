8bcc385deb109db3a029501f672e69f551036515
sallad4 <dallas.gold@gmail.com>
2018-09-07 16:11:37 -0700

    git push

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,95 @@
 import csv
 import statistics
+from collections import Counter
 
-#def findDataCount(bankData):
-    #bankDataCount = csv.reader(bankData, delimiter=",")
+def findDataCount(bankData):
+    with open(bankData, 'r') as csvfile:
+        bank_data = csv.DictReader(csvfile, delimiter=",")
+           
+        count = 0
+        
+        for _ in bank_data:
+            count += 1
+
+        return count
+
+def findPLTotal(bankData):
+    with open(bankData, 'r') as csvfile:
+        bank_data = csv.DictReader(csvfile, delimiter=",")    
     
-    #count = 0
+        plTotal = 0
 
-    #for _ in bankData:
-    #    count += 1
+        for row in bank_data:
+            plTotal += int(row['Profit/Losses'])
 
-    #print(count)
+        return plTotal
 
-#def findPLTotal(bankData):
-    #bank_PL_data = csv.reader(bankData, delimiter=",")
-    #next(bankData)
+def average_change_PL(bankData):
+    with open(bankData, 'r') as csvfile:
+        bank_data = csv.DictReader(csvfile, delimiter=",")     
 
+        monthly_dif_List = []
+        diff = []
+
+        for row in bank_data:
+            monthly_dif_List.append(int(row['Profit/Losses']))
+
+        diff = [monthly_dif_List[i] - monthly_dif_List[i+1] for i in range(len(monthly_dif_List) -1)]
+        
+        avgPL = statistics.mean(diff)
+    
+        return(avgPL)
+
+def find_best_month_amt(bankData):
+    with open(bankData, 'r') as csvfile:
+        bank_data = csv.DictReader(csvfile, delimiter=",")     
+    
+        monthly_dif_List = []
+        diff = []
+
+        for row in bank_data:
+            monthly_dif_List.append(int(row['Profit/Losses']))
+
+        diff = [monthly_dif_List[i] - monthly_dif_List[i+1] for i in range(len(monthly_dif_List) -1)]
+        maxMonth = max(diff) * -1
+ 
+        return(maxMonth)
+
+def find_worst_month_amt(bankData):
+    with open(bankData, 'r') as csvfile:
+        bank_data = csv.DictReader(csvfile, delimiter=",")     
+    
+        monthly_dif_List = []
+        diff = []
+        for row in bank_data:
+            monthly_dif_List.append(int(row['Profit/Losses']))
+
+        diff = [monthly_dif_List[i] - monthly_dif_List[i+1] for i in range(len(monthly_dif_List) -1)]
+        minMonth = min(diff) * -1
+
+        return(minMonth)
+    
+def find_best_month(bankData, bestMonth):
+    with open(bankData, 'r') as csvfile:
+        bank_data = csv.DictReader(csvfile, delimiter=",")
+
+        for row in bank_data:
+            if bestMonth == int(row['Profit/Losses']):
+                return(row['Date'])
+
+def find_worst_month(bankData, worstMonth):
+    with open(bankData, 'r') as csvfile:
+        bank_data = csv.DictReader(csvfile, delimiter=",")
+
+        for row in bank_data:
+            if worstMonth == int(row['Profit/Losses']):
+                return(row['Date'])      
+       
+    
+    
+    #print(maxMonth)
+    #print(minMonth)
+    
     #plTotal = 0
 
     #for row in bankData:
@@ -34,82 +109,34 @@ import statistics
     
 #    for row in bank_data:
 #        print(row['Profit/Losses'])
+def main():
 
-with open('budget_data.csv', 'r') as csvfile:
-    bank_data = csv.DictReader(csvfile, delimiter=",")
+    bank_data_file = 'C:\\Users\\sallad\\Documents\\GitHub\\python-challenge\\pyBank\\budget_data.csv'    
+
+    data_items = findDataCount(bank_data_file)
+    print(data_items)
+
+    profit_loss_total = findPLTotal(bank_data_file)
+    print(profit_loss_total)
+
+    profit_loss_change = average_change_PL(bank_data_file)
+    print(profit_loss_change)
+
+    most_profitable_month = find_best_month_amt(bank_data_file)
+    print(most_profitable_month)
+
+    least_profitable_month = find_worst_month_amt(bank_data_file)
+    print(least_profitable_month)
     
-    count = 0
+    best_calendar_mth = find_best_month(bank_data_file, most_profitable_month)
+    print(best_calendar_mth)
 
-    for _ in bank_data:
-        count += 1
+    worst_calendar_mth = find_worst_month(bank_data_file, least_profitable_month)
+    print(worst_calendar_mth)
 
-    print(count)
+main()
 
-with open('budget_data.csv', 'r') as csvfile:
-    bank_data = csv.DictReader(csvfile, delimiter=",")    
     
-    plTotal = 0
-
-    for row in bank_data:
-        plTotal += int(row['Profit/Losses'])
-    
-    print(plTotal)
-
-with open('budget_data.csv', 'r') as csvfile:
-    bank_data = csv.DictReader(csvfile, delimiter=",")     
-    
-    monthly_PL_List = []
-    monthly_dif_List = []
-    diff = [] 
-    
-    for row in bank_data:
-        monthly_PL_List.append(int(row['Profit/Losses']))
-
-    diff = [monthly_PL_List[i] - monthly_PL_List[i+1] for i in range(len(monthly_PL_List) -1)]
-    maxMonth = max(diff)
-    minMonth = min(diff)
-
-    avgPL = statistics.mean(diff)
-    
-    print(avgPL)
-    print(maxMonth)
-    print(minMonth)
-
-with open('budget_data.csv', 'r') as csvfile:
-    bank_data = csv.DictReader(csvfile, delimiter=",")     
-    
-    maxList, minList, dateList = [], [], []
-
-    for row in bank_data:
-        maxList.append(int(row['Profit/Losses']))
-        minList.append(int(row['Profit/Losses']))
-        dateList.append(row['Date'])
-        
-    maxMonth = max(maxList)
-    minMonth = min(minList)
-
-    counter = 0
-
-    for i in maxList:
-        if int(i) == maxMonth:    
-            print(dateList[counter] + " " + str(maxMonth))
-            break 
-        counter +=1
-
-    counter = 0 
-
-    for i in minList:
-        if int(i) == minMonth:    
-            print(dateList[counter] + " " + str(minMonth))
-            break 
-        counter +=1
-
-    #for i in maxList:
-    ##    if maxMonth == int(i):
-    #        print((dateList[i]))
-    #for _ in minList:
-    #    if minMonth == int(_['Profit/Losses']):
-    #        print('dateList'[_])
 
  
     #findDataCount(bank_data)
