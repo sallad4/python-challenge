264cf1afe773ae719c4a0d5db68d37757b5cd14b
sallad4 <dallas.gold@gmail.com>
2018-09-08 22:07:24 -0700

    add files
    main.py + financial_analysis.txt

--- PyBank/main.py
+++ PyBank/main.py
@@ -33,8 +33,8 @@ def average_change_PL(bankData):
 
         for row in bank_data:
             monthly_dif_List.append(int(row['Profit/Losses']))
-
-        diff = [monthly_dif_List[i] - monthly_dif_List[i+1] for i in range(len(monthly_dif_List) -1)]
+        
+        diff = [-(monthly_dif_List[i]) + monthly_dif_List[i+1] for i in range(len(monthly_dif_List) -1)]
         
         avgPL = statistics.mean(diff)
     
@@ -45,13 +45,13 @@ def find_best_month_amt(bankData):
         bank_data = csv.DictReader(csvfile, delimiter=",")     
     
         monthly_dif_List = []
-        diff = []
+        difference = []
 
         for row in bank_data:
             monthly_dif_List.append(int(row['Profit/Losses']))
 
-        diff = [monthly_dif_List[i] - monthly_dif_List[i+1] for i in range(len(monthly_dif_List) -1)]
-        maxMonth = max(diff) * -1
+        difference = [-(monthly_dif_List[i]) + monthly_dif_List[i+1] for i in range(len(monthly_dif_List) -1)]
+        maxMonth = max(difference)
  
         return(maxMonth)
 
@@ -60,86 +60,102 @@ def find_worst_month_amt(bankData):
         bank_data = csv.DictReader(csvfile, delimiter=",")     
     
         monthly_dif_List = []
-        diff = []
+        difference = []
         for row in bank_data:
             monthly_dif_List.append(int(row['Profit/Losses']))
 
-        diff = [monthly_dif_List[i] - monthly_dif_List[i+1] for i in range(len(monthly_dif_List) -1)]
-        minMonth = min(diff) * -1
+        difference = [-(monthly_dif_List[i]) + monthly_dif_List[i+1] for i in range(len(monthly_dif_List) -1)]
+        minMonth = min(difference)
 
         return(minMonth)
     
-def find_best_month(bankData, bestMonth):
+def find_best_month(bankData, best_month):
     with open(bankData, 'r') as csvfile:
-        bank_data = csv.DictReader(csvfile, delimiter=",")
+        bank_data = csv.DictReader(csvfile, delimiter=",")     
+
+        max_list, date_list, differences = [], [], []
 
         for row in bank_data:
-            if bestMonth == int(row['Profit/Losses']):
-                return(row['Date'])
+            max_list.append(int(row['Profit/Losses']))
+            date_list.append(row['Date'])
 
-def find_worst_month(bankData, worstMonth):
+        differences = [-(max_list[i]) + max_list[i+1] for i in range(len(max_list) -1)]
+
+        data_counter = 0
+
+        for a_diff in differences:
+            data_counter +=1
+            if a_diff == best_month:    
+                return (date_list[data_counter])         
+        
+
+def find_worst_month(bankData, worst_month):
     with open(bankData, 'r') as csvfile:
-        bank_data = csv.DictReader(csvfile, delimiter=",")
+        bank_data = csv.DictReader(csvfile, delimiter=",")     
+
+        min_list, date_list, differences = [], [], []
 
         for row in bank_data:
-            if worstMonth == int(row['Profit/Losses']):
-                return(row['Date'])      
-       
-    
-    
-    #print(maxMonth)
-    #print(minMonth)
-    
-    #plTotal = 0
+            min_list.append(int(row['Profit/Losses']))
+            date_list.append(row['Date'])
 
-    #for row in bankData:
-    #   plTotal += int(row[1])
-    
-    #print(plTotal)
+        differences = [-(min_list[i]) + min_list[i+1] for i in range(len(min_list) -1)]
 
-#The total number of months included in the dataset
-#The total net amount of "Profit/Losses" over the entire period
-#The average change in "Profit/Losses" between months over the entire period
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest decrease in losses (date and amount) over the entire period
+        data_counter = 0
 
-#with open('pyBank/budget_data.csv', 'r') as csvfile:
-#    bank_data = csv.DictReader(csvfile, delimiter=",")
-    #next(bank_data)
-    
-#    for row in bank_data:
-#        print(row['Profit/Losses'])
+        for a_diff in differences:
+            data_counter += 1
+            if a_diff == worst_month:
+                return (date_list[data_counter])
+        
+def print_to_file(data, pl_total, pl_change, good_month, bad_month,good_cal, bad_cal):
+    with open("C:\\Users\\sallad\\Documents\\GitHub\\python-challenge\\pyBank\\financial_analysis.txt", "w") as text_file:
+        print("Financial Analysis")
+        print("-------------------------------------")
+        print(f'Total Months: {data}', file=text_file)
+        print(f'Total: ${pl_total}', file=text_file)
+        print(f'Average Change: ${pl_change}', file=text_file)
+        print(f'Greatest Increase in Profits: {good_cal} (${good_month})', file=text_file) 
+        print(f'Greatest Decrease in Profits: {bad_cal} (${bad_month})', file=text_file)
+        text_file.close()
+
 def main():
-
+    #define CSV path
     bank_data_file = 'C:\\Users\\sallad\\Documents\\GitHub\\python-challenge\\pyBank\\budget_data.csv'    
 
+    #The total number of months included in the dataset
     data_items = findDataCount(bank_data_file)
-    print(data_items)
-
-    profit_loss_total = findPLTotal(bank_data_file)
-    print(profit_loss_total)
-
-    profit_loss_change = average_change_PL(bank_data_file)
-    print(profit_loss_change)
-
-    most_profitable_month = find_best_month_amt(bank_data_file)
-    print(most_profitable_month)
-
-    least_profitable_month = find_worst_month_amt(bank_data_file)
-    print(least_profitable_month)
     
+    #The total net amount of "Profit/Losses" over the entire period
+    profit_loss_total = findPLTotal(bank_data_file)
+    
+    #The average change in "Profit/Losses" between months over the entire period
+    profit_loss_change = average_change_PL(bank_data_file)
+    
+    #The greatest increase in profits (amount) over the entire period
+    most_profitable_month = find_best_month_amt(bank_data_file)
+    
+    #The greatest decrease in losses (amount) over the entire period
+    least_profitable_month = find_worst_month_amt(bank_data_file)
+        
+    #The greatest increase in profits (date) over the entire period
     best_calendar_mth = find_best_month(bank_data_file, most_profitable_month)
-    print(best_calendar_mth)
-
+    
+    #The greatest decrease in losses (date) over the entire period
     worst_calendar_mth = find_worst_month(bank_data_file, least_profitable_month)
-    print(worst_calendar_mth)
+    
+    print("Financial Analysis")
+    print("-------------------------------------")
+    print('Total Months: ' + str(data_items))
+    print('Total: $' + str(profit_loss_total))
+    print('Average Change: $' + str(profit_loss_change))
+    print('Greatest Increase in Profits: ' + best_calendar_mth + " ($" + str(most_profitable_month)+ ")") 
+    print('Greatest Decrease in Profits: ' + worst_calendar_mth + " ($" + str(least_profitable_month)+ ")")
+
+    print_to_file(data_items, profit_loss_total, profit_loss_change, most_profitable_month, least_profitable_month, best_calendar_mth, worst_calendar_mth)    
+
 
 main()
 
-    
-
- 
-    #findDataCount(bank_data)
-    #findPLTotal(bank_data)
 
     
